ba972f6bd2f2dea590526c66317cf37cba69400b
Daniel Zafman <43457022+dzafman1@users.noreply.github.com>
2018-10-23 11:23:32 -0400

    Update Chemical_Code.ino

--- Chemical_Code.ino
+++ Chemical_Code.ino
@@ -6,9 +6,13 @@
 Enes100Simulation enes;
 DFRTankSimulation tank;
 
+
+
+  
 void setup() {
 
   tank.init();
+
 
   enes.println("Starting Navigation");
 
@@ -22,19 +26,13 @@ void loop() {
   int j = false;
   
   //turn to face forward
-  while (abs(enes.location.theta) > 0.05) {
 
-    tank.setLeftMotorPWM(255);
-    tank.setRightMotorPWM(-255);
 
-    while (!enes.updateLocation());
-    
-  }
-
+  locUpdate();
   //move forward
   tank.setLeftMotorPWM(255);
   tank.setRightMotorPWM(255);
-
+while(true){
 if(enes.readDistanceSensor(1) < 0.5 || enes.readDistanceSensor(0) < 0.5 || enes.readDistanceSensor(2) < 0.5){
    enes.println("Found Obstacle");
   tank.setLeftMotorPWM(-255);
@@ -48,11 +46,37 @@ if(enes.readDistanceSensor(1) < 0.5 || enes.readDistanceSensor(0) < 0.5 || enes.
   j  = true;
 }
 while(j){
+  enes.println("SHIT");
     tank.setLeftMotorPWM(255);
     tank.setRightMotorPWM(-255);
     delay(500);
     break;
   }
-  
+  tank.setLeftMotorPWM(255);
+  tank.setRightMotorPWM(255);
+  if(j){
+    j = false;
+  delay(3200);
+  while (!enes.retrieveDestination());
 
+  while (!enes.updateLocation());
+  missionSite();
+  }
+}
+}
+void locUpdate(){
+  while (abs(enes.location.theta) > 0.02) {
+    tank.setLeftMotorPWM(255);
+    tank.setRightMotorPWM(-255);
+    while (!enes.updateLocation());
+    
+  }
+}
+void missionSite(){
+  while (abs(enes.location.theta) > 0.02) {
+    tank.setLeftMotorPWM(255);
+    tank.setRightMotorPWM(-255);
+    while (!enes.retrieveDestination());
+    
+  }
 }
