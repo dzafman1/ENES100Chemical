7c934077ea96c3e5b599a2c4c199b8c551266f54
Daniel Zafman <43457022+dzafman1@users.noreply.github.com>
2018-10-25 10:48:06 -0400

    Update Chemical_Code.ino

--- Chemical_Code.ino
+++ Chemical_Code.ino
@@ -14,45 +14,42 @@ void setup() {
   enes.println("Starting Navigation");
   while (!enes.retrieveDestination());
   while (!enes.updateLocation());
+    missionSite();
 }
 
 void loop() {
-  int j = false;
-  //turn to face forward
-  missionSite();
+
+
   //move forward
   tank.setLeftMotorPWM(255);
   tank.setRightMotorPWM(255);
-while(true){
-if(enes.readDistanceSensor(1) < 0.5 || enes.readDistanceSensor(0) < 0.5 || enes.readDistanceSensor(2) < 0.5){
+while(enes.readDistanceSensor(1) < 0.5 || enes.readDistanceSensor(0) < 0.5 || enes.readDistanceSensor(2) < 0.5){
+
+  
    enes.println("Found Obstacle");
   tank.setLeftMotorPWM(-255);
   tank.setRightMotorPWM(-255);
   delay(200);
   tank.setLeftMotorPWM(0);
    tank.setRightMotorPWM(0);
-  tank.setLeftMotorPWM(-255);
+   enes.updateLocation();
+   if(enes.location.y <= 1){
+     tank.setLeftMotorPWM(-255);
   tank.setRightMotorPWM(255);
-  delay(1000);
-  j  = true;
-}
-while(j){
-  enes.println("SHIT");
+  delay(700);
+   } if(enes.location.y > 1){
     tank.setLeftMotorPWM(255);
-    tank.setRightMotorPWM(-255);
-    delay(500);
-    break;
-  }
+  tank.setRightMotorPWM(-255);
+  delay(700);
+   }
+  
+
   tank.setLeftMotorPWM(255);
   tank.setRightMotorPWM(255);
-  if(j){
-  j = false;
-  delay(3200);
-  while (!enes.retrieveDestination());
-  while (!enes.updateLocation());
+  delay(2000);
   missionSite();
-  }
 }
+
   if(enes.location.x==enes.destination.x && enes.location.y==enes.destination.y){
     tank.setLeftMotorPWM(0);
     tank.setRightMotorPWM(0);
@@ -72,16 +69,16 @@ void missionSite(){
   enes.retrieveDestination();
   float currentX = enes.location.x;
   float currentY = enes.location.y;
-  float currentTheta = floorf(enes.location.theta*100)/100;
+  float currentTheta = enes.location.theta*100/100;
   float destinationX = enes.destination.x;
   float destinationY = enes.destination.y;
   float EPSILON = 0.01;
   float deltY = (destinationY - currentY);
   float deltX = (destinationX - currentX);
-  float destinationTheta = floorf(atan(deltY/ deltX) * 100) / 100;
+  float destinationTheta = atan(deltY/ deltX) * 100 / 100;
   while(destinationTheta != currentTheta){
-    tank.setLeftMotorPWM(30);
-    tank.setRightMotorPWM(-30);
+    tank.setLeftMotorPWM(40);
+    tank.setRightMotorPWM(-40);
     enes.updateLocation();
     enes.retrieveDestination();
     destinationX = enes.destination.x;
@@ -90,15 +87,15 @@ void missionSite(){
     currentY = enes.location.y;
     deltY = (destinationY - currentY);
     deltX = (destinationX - currentX);
-    currentTheta = floorf(enes.location.theta*100)/100;
-    destinationTheta = floorf(atan(deltY/ deltX) * 100) / 100;
+    currentTheta = enes.location.theta*100/100;
+    destinationTheta = atan(deltY/ deltX) * 100 / 100;
     enes.println("");
     enes.print(destinationTheta);
     enes.print("     ");
     enes.print(currentTheta);
-    /*if(currentTheta<=destinationTheta+EPSILON && currentTheta>=destinationTheta-EPSILON){
+    if(currentTheta<=destinationTheta+EPSILON && currentTheta>=destinationTheta-EPSILON){
       break;
-    }*/
+    }
   }
     enes.print("done");
 
