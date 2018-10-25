3a1ec0b29bb8be7e8d608197878a16a39a641d4f
Daniel Zafman <43457022+dzafman1@users.noreply.github.com>
2018-10-25 10:58:26 -0400

    Update Chemical_Code.ino

--- Chemical_Code.ino
+++ Chemical_Code.ino
@@ -23,21 +23,30 @@ void loop() {
   //move forward
   tank.setLeftMotorPWM(255);
   tank.setRightMotorPWM(255);
+
+
+  /************************************************************************************************
+   *If the distance sensor detects an obstacle, first the OSV will move backwards for 0.3 seconds.*                                          
+   *Next, the OSV will decide whether to turn right or left based on its current location.        *
+   *If the OSV is above 1.5 Meters, it will turn to the right. If the OSV is 1.5 meters or below, *
+   *it will turn to the left. The OSV will then move forward in that direction for 1.5 seconds.   *
+   *The method missionSite() will perform to determine the OSV location in comparison to mission  *
+   ***********************************************************************************************/
 while(enes.readDistanceSensor(1) < 0.5 || enes.readDistanceSensor(0) < 0.5 || enes.readDistanceSensor(2) < 0.5){
 
   
    enes.println("Found Obstacle");
   tank.setLeftMotorPWM(-255);
   tank.setRightMotorPWM(-255);
-  delay(200);
+  delay(300);
   tank.setLeftMotorPWM(0);
    tank.setRightMotorPWM(0);
    enes.updateLocation();
-   if(enes.location.y <= 1){
+   if(enes.location.y <= 1.5){
      tank.setLeftMotorPWM(-255);
   tank.setRightMotorPWM(255);
   delay(700);
-   } if(enes.location.y > 1){
+   } if(enes.location.y > 1.5){
     tank.setLeftMotorPWM(255);
   tank.setRightMotorPWM(-255);
   delay(700);
@@ -46,7 +55,7 @@ while(enes.readDistanceSensor(1) < 0.5 || enes.readDistanceSensor(0) < 0.5 || en
 
   tank.setLeftMotorPWM(255);
   tank.setRightMotorPWM(255);
-  delay(2000);
+  delay(1500);
   missionSite();
 }
 
@@ -55,14 +64,6 @@ while(enes.readDistanceSensor(1) < 0.5 || enes.readDistanceSensor(0) < 0.5 || en
     tank.setRightMotorPWM(0);
   }
   
-}
-void locUpdate(){
-  while (abs(enes.location.theta) > 0.02) {
-    tank.setLeftMotorPWM(255);
-    tank.setRightMotorPWM(-255);
-    while (!enes.updateLocation());
-    
-  }
 }
 void missionSite(){
   enes.updateLocation();
