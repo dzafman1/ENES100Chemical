129a584a450d76684eb21230015a2a731f8c53af
dylfurst <dylfurst@msn.com>
2018-11-13 16:25:10 -0500

    Update VisionSystemActual.ino

--- VisionSystemActual.ino
+++ VisionSystemActual.ino
@@ -6,34 +6,57 @@
 Enes100Simulation enes;
 DFRTankSimulation tank;
 
+// motor one (L)
+int enA = 10;
+int in1 = 12;
+int in2 = 8;
+
+// motor two (R)
+int enB = 5;
+int in3 = 7;
+int in4 = 4;
+
+// IR sensor
+int irSensorPin = A1;
+
+boolean obstacle = false;
+
 float locError=.30;
 boolean stuck;
 int stuckInLoop = 0; 
 
+
 void setup() {
   tank.init();
-  enes.println("Starting Navigation");
+  pinMode(irSensorPin, INPUT);
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
   while (!enes.retrieveDestination());
   while (!enes.updateLocation());
   missionSite();
 }
 
-void loop() {
+ /************************************************************************************************
+    If the distance sensor detects an obstacle, first the OSV will move backwards for 0.2 meters.
+    Next, the OSV will decide whether to turn right or left based on its current location.
+    If the OSV is above 1.2 Meters, it will turn to the right. If the OSV is 1.2 meters or below,
+    it will turn to the left. The OSV will then move forward in that direction for 0.4 meters.
+    The method missionSite() will perform to determine the OSV location in comparison to mission
+   ***********************************************************************************************/
 
+void loop() {
+  //calculate distance to destination
   distanceToMission();
   //move forward
   driveForward(.3, enes.location.x, enes.location.y, enes.location.theta);
+  //turn to face destination
   missionSite();
-
-
-  /************************************************************************************************
-    If the distance sensor detects an obstacle, first the OSV will move backwards for 0.3 seconds.
-    Next, the OSV will decide whether to turn right or left based on its current location.
-    If the OSV is above 1.5 Meters, it will turn to the right. If the OSV is 1.5 meters or below,
-    it will turn to the left. The OSV will then move forward in that direction for 1.5 seconds.
-    The method missionSite() will perform to determine the OSV location in comparison to mission
-   ***********************************************************************************************/
-  while(enes.readDistanceSensor(1) < 0.45 || enes.readDistanceSensor(0) < 0.45 || enes.readDistanceSensor(2) < 0.45) {
+  //obstacle is detected
+  while(foundObstacle()==true) {
     enes.updateLocation();
     enes.println("Found Obstacle");
     driveBackward(.2, enes.location.x, enes.location.y, enes.location.theta);
@@ -130,6 +153,7 @@ void missionSite() {
   }
 }
 
+//drive forward a certain distance
 boolean driveForward(float d, float x, float y, float t){
   float destX = x+(d*cos(abs(t)));
   float destY;
@@ -183,7 +207,7 @@ void turnRight(float t){
   }
   }
  
-
+//drive backwards a certain distance
 void driveBackward(float d, float x, float y, float t){
   float backwardsX = x-(d*cos(abs(t)));
   float backwardsY;
@@ -226,3 +250,14 @@ float distanceToMission(){
   float distance=sqrt(pow(deltY, 2)+pow(deltX, 2));
   return distance;
 }
+
+boolean foundObstacle() {
+  int obstacleReading = digitalRead(irSensorPin);
+  if (obstacleReading == LOW) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
